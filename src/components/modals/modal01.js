1924fd635b4578797c8dd6e9d8dc6909987db2a9
Bryan Yuen <singsing0524@gmail.com>
2020-12-30 21:22:00 -0800

    small fix

--- src/components/modals/modal01.js
+++ src/components/modals/modal01.js
@@ -38,15 +38,15 @@ export default class Modal01 extends Component {
                                     <p><b>Image Gallery: </b></p>
                                     <div className="row">
                                         <div className="col-lg-6 img-block">
-                                            <img className="img-fluid rounded" src={process.env.PUBLIC_URL +"/images/portfolio/BCIT_SOCAS/submission.png"} alt="" />
+                                            <img className="img-fluid rounded" src={process.env.PUBLIC_URL +"/images/portfolio/BCIT_SOCAS/submission.PNG"} alt="" />
                                             <i>Submission Form</i>
                                         </div>
                                         <div className="col-lg-6 img-block">
-                                            <img className="img-fluid rounded" src={process.env.PUBLIC_URL +"/images/portfolio/BCIT_SOCAS/login_page.png"} alt="" />
+                                            <img className="img-fluid rounded" src={process.env.PUBLIC_URL +"/images/portfolio/BCIT_SOCAS/login_page.PNG"} alt="" />
                                             <i>Login Screen for Management Portal</i>
                                         </div>
                                         <div className="col-lg-6 img-block">
-                                            <img className="img-fluid rounded" src={process.env.PUBLIC_URL +"/images/portfolio/BCIT_SOCAS/logged_in.png"} alt="" />
+                                            <img className="img-fluid rounded" src={process.env.PUBLIC_URL +"/images/portfolio/BCIT_SOCAS/logged_in.PNG"} alt="" />
                                             <i>Logged-in view</i>
                                         </div>
                                     </div>
